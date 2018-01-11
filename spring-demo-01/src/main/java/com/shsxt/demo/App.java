56b40e03074143d88645a7d7b17e6603495a30e5
yongganzhang <1285963544@qq.com>
2018-01-11 12:06:36 +0800

    修改app

--- spring-demo-01/src/main/java/com/shsxt/demo/App.java
+++ spring-demo-01/src/main/java/com/shsxt/demo/App.java
@@ -4,16 +4,23 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
-	
+
 	public static void main(String[] args) {
-		
+
 		// Spring框架加载配置文件
-	   ApplicationContext act = new ClassPathXmlApplicationContext("spring-config.xml");
-		
-	   // 获取 Spring初始化的bean
-	   Hello hello =  (Hello)act.getBean("hello");
-	   
-	   hello.print();
-	   
+		ApplicationContext act = new ClassPathXmlApplicationContext("spring-config.xml");
+
+		// 获取 Spring初始化的bean
+		Hello hello = (Hello) act.getBean("hello");
+		System.out.println(hello);
+
+		Hello hello1 = (Hello) act.getBean("hello");
+		System.out.println(hello1);
+
+		Hello hello2 = (Hello) act.getBean("hello");
+		System.out.println(hello2);
+
+		hello.print();
+
 	}
 }
