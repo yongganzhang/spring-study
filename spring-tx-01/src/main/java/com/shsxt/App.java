ee116a049e256dd3baed2af2bd654bd58299ca75
yongganzhang <1285963544@qq.com>
2018-01-13 17:48:18 +0800

    修改 bug

--- spring-tx-01/src/main/java/com/shsxt/App.java
+++ spring-tx-01/src/main/java/com/shsxt/App.java
@@ -9,7 +9,6 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		PlatformTransactionManager
 
 		ApplicationContext act = new ClassPathXmlApplicationContext("spring-config.xml");
 
